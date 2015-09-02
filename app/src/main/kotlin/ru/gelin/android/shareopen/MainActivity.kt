3ca45698a76802d432538b75351d4ab5227bf87d
Denis Nelubin <dnelubin@gmail.com>
2015-09-02 21:15:51 +0600

    Converted to Kotlin.

---
+++ app/src/main/kotlin/ru/gelin/android/shareopen/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.gelin.android.shareopen
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+public class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
