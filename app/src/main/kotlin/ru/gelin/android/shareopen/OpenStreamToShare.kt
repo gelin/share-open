7e96536c2268df4f1dec8135749a49b375023078
Denis Nelubin <dnelubin@gmail.com>
2015-09-06 19:35:09 +0600

    Added sharing a stream.

---
+++ app/src/main/kotlin/ru/gelin/android/shareopen/OpenStreamToShare.kt
@@ -0,0 +1,22 @@
+package ru.gelin.android.shareopen
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+
+public class OpenStreamToShare : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = getIntent()
+        if (Intent.ACTION_VIEW == intent.getAction() && null != intent.getData()) {
+            val newIntent = Intent(Intent.ACTION_SEND)
+            newIntent.setType(intent.getType() ?: "*/*")
+            newIntent.putExtra(Intent.EXTRA_STREAM, intent.getData())
+            startActivity(Intent.createChooser(newIntent, getTitle()))
+        }
+        finish()
+    }
+
+}
